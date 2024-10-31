faf0cfcb32222bf3d299f88e0787ec431d402830
Luis Coco Enríquez <LEnriquez@luxoft.com>
2024-10-31 10:37:34 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Azure;
+using Azure.AI.Inference;
+using Microsoft.Extensions.AI;
+
+var endpoint = new Uri("https://models.inference.ai.azure.com");
+var modelId = "gpt-4o";
+
+//https://github.com/marketplace/models
+var credential = new AzureKeyCredential("");
+
+IChatClient client =
+    new ChatCompletionsClient(endpoint, credential)
+        .AsChatClient(modelId);
+
+var response = await client.CompleteAsync("Hello");
+
+Console.WriteLine(response.Message);
